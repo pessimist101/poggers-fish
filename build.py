78a9d340e4d07277d0ceda1037d374c6b675e8c8
Isabella Szemeti <issythegirl@gmail.com>
2020-07-04 23:39:18 +0100

    initial commit
     - reads info from app.js, index.html and style.css
     - uses template.js to update index.js with the resulting  code

---
+++ build.py
@@ -0,0 +1,26 @@
+import re
+import time
+
+with open('./app/index.html','r') as f:
+    html = f.read()
+
+with open('./app/style.css','r') as f:
+    css = f.read()
+
+with open('./app/app.js','r') as f:
+    js = f.read()
+
+with open('./app/template.js','r') as f:
+    template = f.read()
+
+def insert_code(html, css, js, template):
+    html = html.replace('insert css', css).replace('insert js', js)
+    template = template.replace('const someHTML = ``', f"const someHTML = `{html}`")
+    html = template.replace('insert html', html)
+    return html
+
+newHtml = insert_code(html,css,js,template)
+
+with open('index.js','w') as f:
+    f.write(newHtml)
+
